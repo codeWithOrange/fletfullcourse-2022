562471ff20f3ab5abcdc5db26fa8e8ecf8486290
angad <varunaryan.kumarp11@gmail.com>
2022-10-08 08:35:57 +0530

    add 4.column file

---
+++ 4.column.py
@@ -0,0 +1,85 @@
+import flet 
+from flet import Column, Text, Container, colors ,Page,alignment,border
+def main(page:Page):
+    page.title='column tutorial'
+    page.padding=0
+    page.horizontal_alignment='center'
+    page.bgcolor=colors.LIGHT_GREEN_900
+
+
+    c1=Container(
+        content=Column(
+        controls=[
+            Container(
+                content=Text(value='first child'),
+                bgcolor=colors.BROWN_700,
+                width=100,
+                height=100,
+                alignment=alignment.center
+
+            ),
+            Container(
+                content=Text(value='first child'),
+                bgcolor=colors.BROWN_700,
+                width=100,
+                height=100,
+                alignment=alignment.center
+
+            ),
+            Container(
+                content=Text(value='first child'),
+                bgcolor=colors.BROWN_700,
+                width=100,
+                height=100,
+                alignment=alignment.center ,
+    
+
+            ),
+            Container(
+                content=Text(value='first child'),
+                bgcolor=colors.BROWN_700,
+                width=100,
+                height=100,
+                alignment=alignment.center
+
+            ),
+            Container(
+                content=Text(value='first child'),
+                bgcolor=colors.BROWN_700,
+                width=100,
+                height=100,
+                alignment=alignment.center
+
+            ),
+            Container(
+                content=Text(value='first child'),
+                bgcolor=colors.BROWN_700,
+                width=100,
+                height=100,
+                alignment=alignment.center
+
+            ),
+
+        ]
+        ,
+        spacing=1,
+        alignment='spaceEvenly',
+        # expand=True,
+        # horizontal_alignment='center',
+        width=600,
+        height=400,
+        # wrap=True,
+        scroll=True,
+        auto_scroll=True,
+        run_spacing=3,
+     
+    ),
+    border=border.all(width=5),
+    # expand=True,
+
+    )
+
+
+    page.add(c1)
+
+flet.app(target=main)
